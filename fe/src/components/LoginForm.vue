fd6435f9e169c15d8478d3518f80d7798d11348b
Linkim <kimchaelin13@gmail.com>
2021-01-21 14:42:32 +0900

    FE/feature/utils-validations

--- fe/src/components/LoginForm.vue
+++ fe/src/components/LoginForm.vue
@@ -1,28 +1,33 @@
 <template>
-    <div>
-        
-    </div>
+  <div>
+      <h1>인</h1>
+      <!-- <input
+      v-model="email"
+      id="email"
+      type="text"
+      placeholder="이메일"
+      autocapitalize="off"
+      @keyup.enter="Login">
+      <label for="email"></label>
+      <div v-if="error.email">{{ error.email }}</div>
+
+      <input 
+      v-model="password"
+      id="password"
+      type="password"
+      placeholder="비밀번호"
+      @keyup.enter="Login">
+      <label for="password">비밀번호</label>
+      <div v-if="error.password"></div> -->
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'LoginForm',
-    components: { ComponentName },
-    directives: { DirectiveName },
-    data() {
-        return {
-            
-        };
-    },
-    mounted() {
-        
-    },
-    methods: {
-        
-    },
-};
+    
+}
 </script>
 
-<style lang="scss" scoped>
+<style>
 
 </style>
