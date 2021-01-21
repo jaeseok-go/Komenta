5936a651d2dff0ce99838376305613e1ff7121e3
고재석 <goejasuk2@naver.com>
2021-01-21 14:25:14 +0900

    create swagger ui

--- BE/src/main/java/com/komenta/be/controller/MemberController.java
+++ BE/src/main/java/com/komenta/be/controller/MemberController.java
@@ -2,29 +2,26 @@ package com.komenta.be.controller;
 
 import com.komenta.be.model.member.MemberDTO;
 import com.komenta.be.service.MemberService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@RequestMapping(value ="/member")
 public class MemberController{
 
     @Autowired
     MemberService mservice;
 
+    @ApiOperation(value = "login", notes = "로그인")
     @GetMapping("/login")
-    public String checkIDAndPw(String userid, String userpw){
+    public ResponseEntity checkIDAndPw(String userid, String userpw){
 
-        MemberDTO result = mservice.getPw(userid);
-        String resPw = result.getUserpw();
-        if(resPw == null) {
-            return "아이디가 존재하지 않습니다.";
-        }
-        else if(resPw.equals(userpw)) {
-            return "로그인 성공!";
-        } else {
-            return "비밀번호가 틀렸습니다.";
-        }
+        return ResponseEntity.ok(userid);
     }
 
 
