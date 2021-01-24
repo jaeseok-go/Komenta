9cc70c9636d9c38c83f8d60d3da7e2e473410eaa
배상웅 <dentist312@naver.com>
2021-01-24 14:31:42 +0900

    Updated Join Member (Controller, Serivce, Mapper)

--- BE/src/main/java/com/komenta/be/mapper/MemberMapper.java
+++ BE/src/main/java/com/komenta/be/mapper/MemberMapper.java
@@ -6,5 +6,6 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface MemberMapper {
 
+    int joinMember(MemberDTO dto);
     MemberDTO getPw(String userid);
 }
--- BE/src/main/java/com/komenta/be/service/MemberService.java
+++ BE/src/main/java/com/komenta/be/service/MemberService.java
@@ -4,6 +4,7 @@ import com.komenta.be.model.member.MemberDTO;
 
 public interface MemberService{
 
+    public int joinMember(MemberDTO dto);
     public MemberDTO getPw(String userid);
 
 }
