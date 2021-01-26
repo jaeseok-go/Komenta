4709c3054cb63fa3f097993aa18b453262e82f67
김채린 <kimchaelin13@naver.com>
2021-01-26 20:57:01 +0900

    zebal,,

--- fe/src/components/LoginForm.vue
+++ fe/src/components/LoginForm.vue
@@ -81,7 +81,7 @@
           </button>
         </b-form>
         <div class="sign-setting">
-          <button @click="showModalForm">아이디 또는 비밀번호를 잊으셨습니까?</button>
+          <a @click="showModalForm">아이디 또는 비밀번호를 잊으셨습니까?</a>
           <h6>아직 계정이 없으시다면? <router-link to="/signup">회원 가입</router-link></h6>
         </div>
       </b-col>
