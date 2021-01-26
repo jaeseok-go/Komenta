c7c882eaddf92cb8ad897d0ba27845202c90fc1d
고재석 <goejasuk2@naver.com>
2021-01-26 14:34:55 +0900

    관리자 신고 댓글 관리, 사용자 댓글 차단 기능 컨트롤러/매퍼/DTO/서비스 추가

--- BE/src/main/java/com/komenta/be/mapper/AdminMapper.java
+++ BE/src/main/java/com/komenta/be/mapper/AdminMapper.java
@@ -1,5 +1,6 @@
 package com.komenta.be.mapper;
 
+import com.komenta.be.model.admin.ReportListDTO;
 import com.komenta.be.model.member.MemberDTO;
 import com.komenta.be.model.vod.VodDTO;
 import com.komenta.be.model.vod.VodEpisodeDTO;
@@ -23,4 +24,8 @@ public interface AdminMapper {
    public List<VodEpisodeDTO> selectEpisode(int v_id);
    public int updateEpisode(VodEpisodeDTO episode);
    public int deleteEpisode(int ve_id);
+
+   public List<MemberDTO> selectBlockedMember();
+   public int updateCommentPermission(int u_id);
+   public List<ReportListDTO> selectReportedCommentAndReComment();
 }
