ff8cc47dd27f33d5198143548ef1c66f45583c13
박수아 <xlalfskdl@naver.com>
2021-01-27 02:14:34 +0900

    feature/FE/user/vuex

--- fe/src/stores/index.js
+++ fe/src/stores/index.js
@@ -1,43 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { saveAuthToCookie, saveUserToCookie } from '@/utils/cookies'
-import { loginUser } from '@/api/auth'
+import user from "./modules/user.js";
+
 
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state:{
-        userId:"",
-        password: "",
-        passwordConfirm: "",
-        username:"",
-        nickName: "",
-        userPhoneNumber: "",
-        // 추가
-        platform : "",
+    modules: {
+        user,
     },
-    mutations:{
-        setUser(state,payload){
-            state.usename = payload.name;
-            state.platform = payload.platform;
-        },
-        setToken(state, token) {
-            state.token = token
-        },
-        clearToken(state) {
-            state.token = '';
-        }
-    },
-    actions: {
-        async LOGIN({ commit }, userData) {
-            const { data } = await loginUser(userData);
-            commit('setToken', data.token);
-            commit('setUsername', data.user.username);
-            saveAuthToCookie(data.token);
-            saveUserToCookie(data.user.username);
-            return data
-        }
-    }
 });
 
