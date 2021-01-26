844164baf23a30a7cd83004c906de554ae035080
김채린 <kimchaelin13@naver.com>
2021-01-27 00:49:13 +0900

    fe/feature/user-api-modify

--- fe/src/api/auth.js
+++ fe/src/api/auth.js
@@ -2,15 +2,20 @@ import { auth } from './index'
 
 //회원가입 API
 function registerUser(userData) {
-    return auth.post('member/join', userData);
+    return auth.post('join', userData);
 }
 
 //로그인 API
 function loginUser(userData) {
-    return auth.post('member/login', userData);
+    return auth.post('login', userData);
+}
+
+function updateUser(userData) {
+    return auth.post('update', userData);
 }
 
 export {
     registerUser,
-    loginUser
+    loginUser,
+    updateUser
 }
