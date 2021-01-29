c8a5054a1eb594eef89169fdc23ddbfe73aa2e5e
Hyerim Jeong <hrjeong8@gmail.com>
2021-01-29 11:15:00 +0900

    findpw modify

--- fe/src/components/FindPw.vue
+++ fe/src/components/FindPw.vue
@@ -100,7 +100,7 @@ export default {
           },
         submitChangePw() {
           const userData = {
-            ...this.$store.state.userInfo,
+            // ...this.$store.state.userInfo,
             u_email:this.userId,
             u_pw: this.newPw,
           };
