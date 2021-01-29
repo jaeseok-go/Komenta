89cd4868f0c814a779650f972a95e295d881400c
Linkim <kimchaelin13@gmail.com>
2021-01-29 12:22:54 +0900

    title-logo-temp

--- fe/src/views/user/Login.vue
+++ fe/src/views/user/Login.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <div class="temp-bc-title">
+            <img src="@/imgs/3.png" alt="">
             <h1 class="title-logo">Komenta</h1>
             <hr>
         </div>
@@ -107,6 +108,14 @@ export default {
 
 <style scoped>
   .temp-bc-title {
+    display: flex;
     background-color: #F9F9F9;
+  }
+
+  img {
+    margin-left: 10px;
+    margin-top: 10px;
+    height: 75px;
+    margin-bottom: 10px;
   }
 </style>
