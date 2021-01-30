c6a047d1f1ff725225523731c8ce05f0dee3c119
박수아 <xlalfskdl@naver.com>
2021-01-30 16:43:17 +0900

    feature/FE/VodDetail-skeleton

---
+++ fe/src/components/vod/VodAllEpi.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>VOD 전체회차정보
+    4. 전체회차 탭<br>
+        GET(v_id) -> res(vod 전체회차-회차최신순,날짜,vod회차세부내용)<br>
+      <!-- 받은 데이터 v-for -->
+      <div>
+        vod 포스터, 회차최신순, 회차 세부내용
+      </div>
+  </div>
+  
+  
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      vodInfo: {}
+    }
+  },
+  created() {
+  // this.vodInfo = getVod(v_id);
+
+  }
+}
+</script>
+
+<style>
+
+</style>
