cdbc60f910f6e49d6ccd61af871313d5349442ab
김채린 <kimchaelin13@gmail.com>
2021-01-30 21:15:02 +0900

    FE/feature/Main-vodsection-draft2

---
+++ fe/src/components/Main/VodComment.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>댓글순</h1>
+        <div>this.commentVODs 뿌려주기</div>
+    </div>
+  
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            commentVODs:[]
+        }
+    },
+    methods:{
+        //getcommentVOD api만들어야함
+        fetchCommentVOD() {
+            this.commentVODs = getcommentVOD()
+        }
+    },
+    created(){
+        //댓글순 vod get
+        this.fetchCommentVOD()
+    },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ fe/src/components/Main/VodPopular.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>인기순</h1>
+        <div>this.popularVODs 뿌려주기</div>
+    </div>
+  
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            popularVODs:[]
+        }
+    },
+    methods:{
+        //getPopularVOD api만들어야함
+        fetchPopularVOD() {
+            this.popularVODs = getPopularVOD()
+        }
+    },
+    created(){
+        //인기순 vod get
+        this.fetchPopularVOD()
+    },
+
+}
+</script>
+
+<style>
+
+</style>
