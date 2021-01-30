b7a15eae8f6ad61e8d2d25403af8f50ec1a649c9
김채린 <kimchaelin13@gmail.com>
2021-01-30 22:38:46 +0900

    FE/feture/MyStreamingList-draft1

--- fe/src/components/MyStreamingList/MyStramingList.vue
+++ fe/src/components/MyStreamingList/MyStramingList.vue
@@ -1,26 +1,63 @@
 <template>
     <div>
         <h1>마이 스트리밍 리스트</h1>
-        <!-- asidebar -->
-        <!-- header -->
-        <!-- recently played 스트리밍 리스트 -->
-        <div>내가 시청한 콘텐츠 목록</div>
-        시청기록 테이블
-        GET(u_id) -> 조회 get방식 전부 다 불러오기
-        vh_id, vh_wathching_time, vh_score, ve_id, vh_good,vh_score->vh_name???
-
+        <Header></Header>
+        <AsideBar></AsideBar>
+        <div>Recently Played</div>
+        <ul>
+            <li v-for="recentVOD in recentVODS" :key="recentVOD.id"></li>
+        </ul>
+        
         <div> 좋아요 누른 스트리밍 리스트 </div>
         <!-- u_id => pl_id, pl_name, u_id(플레이리스트 작성자), pl_pic(플레이리스트 대표 사진), ve_id(플레이 리스트 각각 vod 에피소드 id) -->
+        <ul>
+            <li v-for="likePlayList in likePlayLists" :key="likePlayList.id"></li>
+        </ul>
 
     </div>
   
 </template>
 
 <script>
+import { mapState } from 'vuex';
+import AsideBar from '@/components/common/Asidebar'
+import Header from '@/components/common/Header'
+
 export default {
+    components: {
+        AsideBar,
+        Header
+    },
+    data() {
+        return {
+            recentVODs:[],
+            likePlayLists:[],
+        }
+    },
+    computed: {
+    ...mapState({
+      userInfo: state => state.user.userInfo
+    }),
+    methods: {
+        fetchRecentPlaylist() {
+            const u_id = userInfo.u_id
+            // const u_id = store.userInfo.u_id(helper method 안쓰고?)
+            this.recentVODs = getRecentPlaylist(u_id)
+        },
+        fetchLikePlayList() {
+            const u_id = userInfo.u_id
+            this.likePlayLists = getLikePlayList(u_id)
+        }
+
+    },
+    created() {
+        this.fetchRecentPlaylist(),
+        this.fetchLikePlayList()
+    },
 
 }
 </script>
+
 
 <style>
 
