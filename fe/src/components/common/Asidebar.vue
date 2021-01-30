2986dcb3dbd6c662996154bc1cbd5aa16f45712d
Hyerim Jeong <hrjeong8@gmail.com>
2021-01-30 13:15:43 +0900

    vodDetail layout

--- fe/src/components/common/Asidebar.vue
+++ fe/src/components/common/Asidebar.vue
@@ -1,18 +1,18 @@
 <template>
-    <div>
-        사이드바
-        1. Komenta로고
+    <div id="app">
+        사이드바 <br>
+        1. Komenta로고 <br>
         <!-- 로그인전 / 로그인시 store에 저장된 값 가져오기 -->
-        2. 회색사람 아이콘 / 유저 프로필사진
-        3. 로그인 후 이용해 주세요(로그인으로 가는 a링크) / 유저 닉네임
-        4. 메뉴 / 로그인 안하면로그인페이지로 이동!
-        1) Home -> mainpage로 이동
-        2) My StreamingList -> StreamingList로 이동
-        3) VOD -> 카테고리 페이지로 이동
-        4) 마이페이지 -> 마이페이지로 이동
-        5) 내피드 -> myFeed로 이동
-        6) 고객센터 -> 아직 안정함
-        7) 로그아웃
+        2. 회색사람 아이콘 / 유저 프로필사진 <br>
+        3. 로그인 후 이용해 주세요(로그인으로 가는 a링크) / 유저 닉네임 <br>
+        4. 메뉴 / 로그인 안하면로그인페이지로 이동! <br>
+        1) Home -> mainpage로 이동 <br>
+        2) My StreamingList -> StreamingList로 이동 <br>
+        3) VOD -> 카테고리 페이지로 이동 <br>
+        4) 마이페이지 -> 마이페이지로 이동 <br>
+        5) 내피드 -> myFeed로 이동 <br>
+        6) 고객센터 -> 아직 안정함 <br>
+        7) 로그아웃 <br>
     </div>
 </template>
 
@@ -34,6 +34,10 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+    #app {
+        width: 200px;
+        display: inline-block;
+        background-color: yellowgreen;
+    }
 </style>
--- fe/src/views/vod/VodDetail.vue
+++ fe/src/views/vod/VodDetail.vue
@@ -1,23 +1,25 @@
 <template>
     <div>
-        <Header></Header>
         <!-- 사이드바는 hidden -->
         <Asidebar></Asidebar>
+        <Header></Header>
         <!-- vod -->
-        1-1. 해당회차 VOD보여주기
-        GET(ve_id) -> res(비디오 불러오기)
-        1-2. 해당회차 세부내용
-        GET(ve_id) -> res(v_id,회차,날짜,vod회차세부내용,)
-        3. 해당 VOD 전체 세부 내용
-        GET(v_id) -> res(vod이름(v_title),vod세부내용,vod장르 대분류/소분류,vod출연진,vod감독,)
-        4. 전체회차 탭
-        GET(v_id) -> res(vod 전체회차-회차최신순,날짜,vod회차세부내용)
-        5-1. 해당회차 베스트댓글 탭
-        GET(ve_id) -> res(좋아요순 vod회차 댓글정보(c_id,vod회차 댓글내용,댓글유저,댓글좋아요수,댓글생성시간))
-        5-2. 베스트 댓글 누르면 해당 시간대로 이동
-        GET(c_id) -> res(c_playtime)으로 이동???
-        6. 해당시간 댓글 보여주는 컴포넌트
-        GET(ve_id)-> res(댓글유저, 댓글내용, 좋아요수,댓글시간정보,playtime)
+        <div id="appBody">
+            1-1. 해당회차 VOD보여주기 <br>
+            GET(ve_id) -> res(비디오 불러오기)<br>
+            1-2. 해당회차 세부내용<br>
+            GET(ve_id) -> res(v_id,회차,날짜,vod회차세부내용,)<br>
+            3. 해당 VOD 전체 세부 내용<br>
+            GET(v_id) -> res(vod이름(v_title),vod세부내용,vod장르 대분류/소분류,vod출연진,vod감독,)<br>
+            4. 전체회차 탭<br>
+            GET(v_id) -> res(vod 전체회차-회차최신순,날짜,vod회차세부내용)<br>
+            5-1. 해당회차 베스트댓글 탭<br>
+            GET(ve_id) -> res(좋아요순 vod회차 댓글정보(c_id,vod회차 댓글내용,댓글유저,댓글좋아요수,댓글생성시간))<br>
+            5-2. 베스트 댓글 누르면 해당 시간대로 이동<br>
+            GET(c_id) -> res(c_playtime)으로 이동???<br>
+            6. 해당시간 댓글 보여주는 컴포넌트<br>
+            GET(ve_id)-> res(댓글유저, 댓글내용, 좋아요수,댓글시간정보,playtime)<br>
+        </div>
     </div>
 </template>
 
@@ -45,6 +47,12 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+    #appBody {
+        display: inline-block;
+        position: fixed;
+        top: 100px;
+        background-color: red;
+        width: 100%;
+    }
 </style>
