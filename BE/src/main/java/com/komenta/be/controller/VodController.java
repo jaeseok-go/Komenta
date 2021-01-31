5c53606506e748be192f85ec045ac9529243f263
배상웅 <dentist312@naver.com>
2021-01-31 21:28:20 +0900

    After click vod, get Vod(episode) Info

--- BE/src/main/java/com/komenta/be/controller/VodController.java
+++ BE/src/main/java/com/komenta/be/controller/VodController.java
@@ -5,6 +5,7 @@ import com.komenta.be.model.vod.VodHistoryDTO;
 import com.komenta.be.service.AdminService;
 import com.komenta.be.service.VodService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,19 +24,23 @@ public class VodController {
         // 회원 정보를 받는다.
         // 1. jwt에서 u_id값을 받아오는 방법
         // 2. 실패시 그냥 u_id를 파라미터로 받아오는 방법
-
+        HttpStatus status = null;
         // 회원이 봤던거 인지 아닌지
         // 봤으면 시청 시간을 같이 response해주어야함.
-        VodHistoryDTO history = vodService.selectHistoryById(u_id);
-
-        // episode에 대한 정보를 모두 받아옴
-        VodEpisodeAllDTO episode = vodService.selectEpisodeById(ve_id);
-
         Map<String, Object> resultMap = new HashMap<>();
-        resultMap.put("vh_id", history.getVh_id());
-        resultMap.put("vh_wathching_time", history.getVh_watching_time());
-        resultMap.put("episodeInfo", episode);
+        try {
+            VodHistoryDTO history = vodService.selectHistoryById(u_id);
+            // episode에 대한 정보를 모두 받아옴
+            VodEpisodeAllDTO episode = vodService.selectEpisodeById(ve_id);
 
-        return new ResponseEntity<Map<String, Object>(resultMap);
+            resultMap.put("vh_id", history.getVh_id());
+            resultMap.put("vh_wathching_time", history.getVh_watching_time());
+            resultMap.put("episodeInfo", episode);
+        }
+        catch(RuntimeException e){
+            resultMap.put("message", e.getMessage());
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return new ResponseEntity<Map<String, Object>>(resultMap,status);
     }
 }
