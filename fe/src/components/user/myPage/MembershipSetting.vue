fe8b4287488afe722f3132db4ce4f3752a08eec0
Hyerim Jeong <hrjeong8@gmail.com>
2021-01-31 22:59:12 +0900

    myPage modify

--- fe/src/components/user/myPage/MembershipSetting.vue
+++ fe/src/components/user/myPage/MembershipSetting.vue
@@ -1,16 +1,59 @@
 <template>
   <b-col>
     <h4>멤버십 관리</h4>
+    <hr>
+    <table>
+      <tr>
+        <td>멤버십</td>
+        <td>사용 기간</td>
+        <td>결제 예정일</td>
+        <td>상태</td>
+      </tr>
+      <tr v-if="!membership">
+        <td colspan="5">
+          사용중인 멤버십이 없습니다.<br>
+          <a href="#"><h6>멤버십 가입하러 가기</h6></a>
+        </td>
+      </tr>
+      <!-- DB에 저장된 값 불러오기 기능 -->
+      <tr v-else>
+        <td>{{membership.m_name}}</td>
+        <td>{{membership.m_use_date}}</td>
+        <td>{{membership.m_expire_date}}</td>
+        <td @click="changeState">{{membership.m_state}}</td>
+      </tr>
+    </table>
+<!-- 
     1. 멤버십 명 <br>
     2. 사용기간<br>
     3. 결제 예정일<br>
     4. 상태(해지하기, 해지취소) 버튼<br>
+-->
   </b-col>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      membership: {
+        m_name:'1달 정기권',
+        m_use_date:'2021-01-20',
+        m_expire_date:'2021-01-21',
+        m_state:'해지하기'
+      },
+      stateBtnColor:'red'
+    }
+  },
+  methods: {
+    changeState(){
+      if(this.membership.m_state == '해지하기') {
+        this.membership.m_state = '해지취소';
+      }else {
+        this.membership.m_state = '해지하기';
+      }
+    }
+  },
 }
 </script>
 
--- fe/src/components/user/myPage/WatchedVOD.vue
+++ fe/src/components/user/myPage/WatchedVOD.vue
@@ -6,27 +6,20 @@
       <button @click="showModalForm">더보기</button>
     </div>
     <!-- vueper Slides 이용 -->
-      <vueper-slides
-        class="no-shadow"
-        :visible-slides="5"
-        :slide-ratio="1 / 4"
-        :dragging-distance="70"
-        :bullets="false">
-        <vueper-slide v-for="i in 10" :key="i" :title="i.toString()" :style="'background-color: ' + ['#ff5252', '#42b983'][i % 2]" />
-      </vueper-slides>
-
-    <!-- <div class="watchedVOD" v-for="(vod, index) in watchedVODList" :key="index"> -->
-      <!-- 원형으로 포스터 표시 -->
-      <!-- <div class="watchedVODImg">
-        <img :src=vod.v_img>
+    <div class="VODCard">
+      <div class="watchedVOD" v-for="(vod, index) in watchedVODList" :key="index">
+        <!-- 원형으로 포스터 표시 -->
+        <div class="watchedVODImg">
+          <img :src=vod.v_img>
+        </div>
+        <div class="watchedVODText">
+          {{vod.v_name}} {{vod.v_ep_num}}화<br>
+          일시 º {{vod.watched_date}}
+          <!-- VOD명 + 회차 정보 -->
+          <!-- 최근 시청 날짜 -->
+        </div>
       </div>
-      <div class="watchedVODText">
-        {{vod.v_name}} {{vod.v_ep_num}}화<br>
-        일시 º {{vod.watched_date}} -->
-        <!-- VOD명 + 회차 정보 -->
-        <!-- 최근 시청 날짜 -->
-      <!-- </div>
-    </div> -->
+    </div>
   <!-- 
     1. 이미지(포스터...?)<br>
     2. VOD 명 + 회차 정보<br>
@@ -37,17 +30,29 @@
 </template>
 
 <script>
-import {VueperSlides, VueperSlide } from 'vueperslides'
-import 'vueperslides/dist/vueperslides.css'
+// import {VueperSlides, VueperSlide } from 'vueperslides'
+// import 'vueperslides/dist/vueperslides.css'
 
 export default {
-  components:{
-    VueperSlides,
-    VueperSlide
-  },
+  // components:{
+  //   VueperSlides,
+  //   VueperSlide
+  // },
   data() {
     return {
       watchedVODList: [
+        {
+          v_img:'#',
+          v_name:'경이로운 소문',
+          v_ep_num:'21',
+          watched_date:'2021-01-20'
+        },
+        {
+          v_img:'#',
+          v_name:'경이로운 소문',
+          v_ep_num:'21',
+          watched_date:'2021-01-20'
+        },
         {
           v_img:'#',
           v_name:'경이로운 소문',
@@ -82,6 +87,10 @@ export default {
     text-align: right;
   }
 
+  .VODCard {
+    text-align: center;
+  }
+
   .watchedVOD {
     width: 150px;
     display: inline-block;
@@ -95,5 +104,9 @@ export default {
 
   .watchedVODText {
     text-align: center;
+  }
+
+  .vueperslider__content-wrapper {
+    margin-right: 1rem;
   }
 </style>
