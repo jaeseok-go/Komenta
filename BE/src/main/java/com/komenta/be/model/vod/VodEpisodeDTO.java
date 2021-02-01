41c305b2d376292c54e2588a8fcf9237d9feb7c8
고재석 <goejasuk2@naver.com>
2021-02-01 17:56:43 +0900

    vod episode dto 일부 수정

--- BE/src/main/java/com/komenta/be/model/vod/VodEpisodeDTO.java
+++ BE/src/main/java/com/komenta/be/model/vod/VodEpisodeDTO.java
@@ -2,13 +2,30 @@ package com.komenta.be.model.vod;
 
 public class VodEpisodeDTO {
     private int ve_id;
-    private int v_id;
     private int ve_episode_num;
     private String ve_contents;
     private String ve_upload_date;
     private String ve_admin;
+    private int v_id;
+    private String v_title;
+    private int gd_id;
+    private String g_name;
+    private String gd_name;
 
     public VodEpisodeDTO() {
+    }
+
+    public VodEpisodeDTO(int ve_id, int ve_episode_num, String ve_contents, String ve_upload_date, String ve_admin, int v_id, String v_title, int gd_id, String g_name, String gd_name) {
+        this.ve_id = ve_id;
+        this.ve_episode_num = ve_episode_num;
+        this.ve_contents = ve_contents;
+        this.ve_upload_date = ve_upload_date;
+        this.ve_admin = ve_admin;
+        this.v_id = v_id;
+        this.v_title = v_title;
+        this.gd_id = gd_id;
+        this.g_name = g_name;
+        this.gd_name = gd_name;
     }
 
     public VodEpisodeDTO(int ve_id, int v_id, int ve_episode_num, String ve_contents, String ve_admin, String ve_upload_date) {
@@ -83,13 +100,51 @@ public class VodEpisodeDTO {
         this.ve_contents = ve_contents;
     }
 
+    public String getV_title() {
+        return v_title;
+    }
+
+    public void setV_title(String v_title) {
+        this.v_title = v_title;
+    }
+
+    public int getGd_id() {
+        return gd_id;
+    }
+
+    public void setGd_id(int gd_id) {
+        this.gd_id = gd_id;
+    }
+
+    public String getG_name() {
+        return g_name;
+    }
+
+    public void setG_name(String g_name) {
+        this.g_name = g_name;
+    }
+
+    public String getGd_name() {
+        return gd_name;
+    }
+
+    public void setGd_name(String gd_name) {
+        this.gd_name = gd_name;
+    }
+
     @Override
     public String toString() {
-        return "VodEpisodeTableDTO{" +
+        return "VodEpisodeDTO{" +
                 "ve_id=" + ve_id +
-                ", v_id=" + v_id +
                 ", ve_episode_num=" + ve_episode_num +
                 ", ve_contents='" + ve_contents + '\'' +
+                ", ve_upload_date='" + ve_upload_date + '\'' +
+                ", ve_admin='" + ve_admin + '\'' +
+                ", v_id=" + v_id +
+                ", v_title='" + v_title + '\'' +
+                ", gd_id=" + gd_id +
+                ", g_name='" + g_name + '\'' +
+                ", gd_name='" + gd_name + '\'' +
                 '}';
     }
 }
