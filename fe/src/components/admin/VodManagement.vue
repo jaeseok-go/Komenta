7b55e5b5b78d575c512aa93386924b682e44a2d3
Hyerim Jeong <hrjeong8@gmail.com>
2021-02-01 18:41:25 +0900

    admin VodManagement create

--- fe/src/components/admin/VodManagement.vue
+++ fe/src/components/admin/VodManagement.vue
@@ -1,27 +1,31 @@
 <template>
   <b-container>
     <h4>전체 VOD</h4>
-    <table>
-      <tr>
-        <td>등록번호</td>
-        <td>제목</td>
-        <td>종류/장르</td>
-        <td>등록일</td>
-        <td>담당자</td>
-      </tr>
-      <tr v-if="vodList.length == 0">
+    <table style="text-align:center;">
+      <colgroup>
+        <col width="10%" />
+        <col width="15%" />
+        <col width="45%" />
+        <col width="30%" />
+      </colgroup>
+      <thead>
+        <th>등록번호</th>
+        <th>이미지</th>
+        <th>제목</th>
+        <th>종류/장르</th>
+      </thead>
+      <tbody v-if="vodList.length == 0">
         <td colspan="4">
           등록된 VOD가 없습니다.
         </td>
-      </tr>
+      </tbody>
       <!-- 페이징 처리...어케해... -->
-      <tr v-for="(vod, index) in vodList" :key="index" v-else>
+      <tbody v-for="(vod, index) in vodList" :key="index" v-else>
         <td>{{vod.v_id}}</td>
+        <td><img :src="getPicPath(index)" width="50%"></td>
         <td>{{vod.v_title}}</td>
         <td>{{vod.g_name}}/{{vod.gd_name}}</td>
-          <!-- 등록일, 담당자 추가 -->
-        <!-- <td>{{vod.}}</td> -->
-      </tr>
+      </tbody>
     </table>
     <br>
   </b-container>
@@ -42,6 +46,11 @@ export default {
     async getVODList() {
       const response = await fetchAllVOD();
       console.log("vod list : ",response);
+      this.vodList = response.data;
+    },
+    getPicPath(index){
+      const picPath = require(`@/assets/images/${this.vodList[index].v_poster}.png`);
+      return picPath;
     }
   },
 }
