437dd0a0b5391e5534d780e1a63ead475d03a68d
김채린 <kimchaelin13@naver.com>
2021-02-01 22:13:58 +0900

    [FIX]폴더 이름 변경

--- fe/src/App.vue
+++ fe/src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
     <router-view></router-view>
+    <Main></Main>
   </div>
 </template>
 
 <script>
 // import "@/css/index.css"
+import Main from '@/components/Main/Main'
 
 export default {
+  components: {
+    Main
+  },
   name: 'App',
 }
 </script>
