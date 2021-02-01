3fb010486c48baf977a724b3e69426d813bafc4f
고재석 <goejasuk2@naver.com>
2021-02-01 15:54:49 +0900

    장르 컨트롤러에 cross origin 추가

--- BE/src/main/java/com/komenta/be/controller/GenreController.java
+++ BE/src/main/java/com/komenta/be/controller/GenreController.java
@@ -7,12 +7,14 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/genre")
 public class GenreController {
