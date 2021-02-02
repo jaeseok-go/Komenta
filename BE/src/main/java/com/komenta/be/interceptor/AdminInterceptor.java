79f29df5b7d9b634f5c626cd4ddc640ac7ef5ddc
배상웅 <dentist312@naver.com>
2021-02-02 16:18:12 +0900

    Change Admin interceptor

--- BE/src/main/java/com/komenta/be/interceptor/AdminInterceptor.java
+++ BE/src/main/java/com/komenta/be/interceptor/AdminInterceptor.java
@@ -5,17 +5,18 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.komenta.be.service.JwtService;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import java.util.Map;
+
 @Component
 public class AdminInterceptor implements HandlerInterceptor {
-
-    @Autowired
-    private JwtService jwtService;
 
     public static final Logger logger = LoggerFactory.getLogger(JwtInterceptor.class);
 
@@ -23,11 +24,24 @@ public class AdminInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         logger.info(request.getMethod() + " : " + request.getServletPath());
         String token = request.getHeader("auth-token");
-        String claim = (String) jwtService.get(token).get("is_admin");
-        if(claim.equals("true")){
-            return true;
-        }
-        return false;
+        return getUidFromJwt(token);
+    }
 
+    public Map<String, Object> get(String jwt) {
+        Jws<Claims> claims = null;
+        try {
+            claims = Jwts.parser().setSigningKey("VUETOKEN".getBytes()).parseClaimsJws(jwt);
+        } catch (final Exception e) {
+            throw new RuntimeException();
+        }
+
+        logger.info("claims : {}", claims);
+        // Claims는 Map의 구현체이다.
+        return claims.getBody();
+    }
+
+    public boolean getUidFromJwt(String jwt){
+        boolean admin = (boolean) get(jwt).get("is_admin");
+        return admin;
     }
 }
