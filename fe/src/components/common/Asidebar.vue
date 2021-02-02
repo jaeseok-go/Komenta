358fd0dec2bd909f55a80263120b05c615570ee0
김채린 <kimchaelin13@gmail.com>
2021-02-02 16:34:12 +0900

    [REFACTOR]ASIDEBAR CSS,ROUTES 연결

--- fe/src/components/common/Asidebar.vue
+++ fe/src/components/common/Asidebar.vue
@@ -1,75 +1,79 @@
 <template>
-        <!-- 사이드바 <br>
-        1. Komenta로고 <br>
-        로그인전 / 로그인시 store에 저장된 값 가져오기
-        2. 회색사람 아이콘 / 유저 프로필사진 <br>
-        3. 로그인 후 이용해 주세요(로그인으로 가는 a링크) / 유저 닉네임 <br>
-        4. 메뉴 / 로그인 안하면로그인페이지로 이동! <br>
-        1) Home -> mainpage로 이동 <br>
-        2) My StreamingList -> StreamingList로 이동 <br>
-        3) VOD -> 카테고리 페이지로 이동 <br>
-        4) 마이페이지 -> 마이페이지로 이동 <br>
-        5) 내피드 -> myFeed로 이동 <br>
-        6) 고객센터 -> 아직 안정함 <br>
-        7) 로그아웃 <br> -->
+    <div class='whole-border'>
 
-    <div id="#app">
-        <v-app id="inspire">
-            <v-card
-            width="256"
-            height="70rem"
-            tile
-        >
-            <v-navigation-drawer permanent>
-                <h1>Komenta</h1>
-                <v-list>
-                    <v-list-item>
-                        <v-list-item-avatar>
-                            <v-img src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
-                        </v-list-item-avatar>
-                    </v-list-item>
-  
-                    <v-list-item link>
-                        <v-list-item-content>
-                            <v-list-item-title class="title">
-                                John Leider
-                            </v-list-item-title>
-                            <v-list-item-subtitle>john@vuetifyjs.com</v-list-item-subtitle>
-                        </v-list-item-content>
-  
-                        <v-list-item-action>
-                            <v-icon>mdi-menu-down</v-icon>
-                        </v-list-item-action>
-                        </v-list-item>
-                        </v-list>
-                        <v-divider></v-divider>
-                        <v-list
-                            nav
-                            dense
-                        >
-                        <v-list-item-group
-                            v-model="selectedItem"
-                            color="primary"
-                        >
-                        <v-list-item
-                        v-for="(item, i) in items"
-                        :key="i"
-                        >
-                        <v-list-item-icon>
-                            <v-icon v-text="item.icon"></v-icon>
-                        </v-list-item-icon>
-  
-                        <v-list-item-content>
-                            <v-list-item-title v-text="item.text"></v-list-item-title>
-                        </v-list-item-content>
-                    </v-list-item>
-          </v-list-item-group>
-        </v-list>
-      </v-navigation-drawer>
-    </v-card>
-  </v-app>
+    <div class="sidebar">
+            <div class="sidebar-logo">
+                <span>Komenta</span>
+            </div>
+            <div class="sidebar-profile">
+                <div class="sidebar-profile-pic"><i class="fad fa-user-circle"></i></div>
+                <span class="sidebar-profilc-nickname">닉네임은 뭘로 하지</span>
+                <span class="sidebar-profilc-id">ssafy@komenta.com</span>
+                
+            </div>
+            <ul class="sidebar-ul">
+              <li class="sidebar-child">
+                <router-link :to="{name:'Main'}">
+                  <span class="sidebar-title">
+                    <span class="sidebar-title-icon icon-bg1"><ion-icon name="person-circle"></ion-icon></span>
+                    <span class="sidebar-title-text">Home</span>
+                  </span>
+                </router-link>
+                
+              <li class="sidebar-child">
+                <router-link :to="{name:'MyPlayList'}">
+                  <span class="sidebar-title">
+                    <span class="sidebar-title-icon icon-bg2"><ion-icon name="albums-outline"></ion-icon></span>
+                    <span class="sidebar-title-text">Subscriptions</span>
+                  </span>
+                </router-link>
+                
+              <li class="sidebar-child">
+                <router-link :to="{name:'Category'}">
+                  <span class="sidebar-title">
+                    <span class="sidebar-title-icon icon-bg3"><ion-icon name="caret-forward-outline"></ion-icon></span>
+                    <span class="sidebar-title-text">ALL Video</span>
+                  </span>
+                </router-link>
+
+              <li class="sidebar-child">
+                <router-link :to="{name:'Main'}">
+                  <span class="sidebar-title">
+                    <span class="sidebar-title-icon icon-bg4"><ion-icon name="globe-outline"></ion-icon></span>
+                    <span class="sidebar-title-text">People</span>
+                  </span>
+                </router-link>
+              </li>
+              <li class="sidebar-child">
+                <router-link :to="{name:'Feed'}">
+                  <span class="sidebar-title">
+                    <span class="sidebar-title-icon icon-bg5"><ion-icon name="share-social-outline"></ion-icon></span>
+                    <span class="sidebar-title-text">My Feed</span>
+                  </span>
+                </router-link>
+
+                <li class="sidebar-child">
+                <router-link :to="{name:'Main'}">
+                  <span class="sidebar-title">
+                    <span class="sidebar-title-icon icon-bg6"><ion-icon name="settings-outline"></ion-icon></span>
+                    <span class="sidebar-title-text">Settings</span>
+                  </span>
+                </router-link>
+
+                <li class="sidebar-child">
+                <router-link :to="{name:'Main'}">
+                  <span class="sidebar-title">
+                    <span class="sidebar-title-icon icon-bg7"><ion-icon name="cloud"></ion-icon></span>
+                    <span class="sidebar-title-text">Help</span>
+                  </span>
+                </router-link>
+
+                
+ 
+              </li>
+            </ul>
+      </div>
     </div>
-
 </template>
 
 <script>
@@ -77,31 +81,5 @@
 export default {
     // el: '#app',
 
-    name: 'asidebar',
-    data:() => ({
-        selectedItem: 0,
-        items: [
-        { text: 'Home', icon: 'home' },
-        { text: 'My Streaming List', icon: 'album' },
-        { text: 'VOD', icon: 'ondemand_video' },
-        { text: 'My Feed', icon: 'mdi-check-circle' },
-        { text: 'My Page', icon: 'account_circle' },
-        { text: '고객센터', icon: 'support_agent' },
-        ],
-    }),
-    mounted() {
-        
-    },
-    methods: {
-        
-    },
 };
 </script>
-
-<style scoped>
-    /* #app {
-        width: 200px;
-        display: inline-block;
-        background-color: yellowgreen;
-    } */
-</style>
