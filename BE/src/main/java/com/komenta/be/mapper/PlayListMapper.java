348f494c5e231da37396a13a51f8e2834e9e70ce
배상웅 <dentist312@naver.com>
2021-02-04 11:04:56 +0900

    Build PlayList

--- BE/src/main/java/com/komenta/be/mapper/PlayListMapper.java
+++ BE/src/main/java/com/komenta/be/mapper/PlayListMapper.java
@@ -2,6 +2,9 @@ package com.komenta.be.mapper;
 
 import com.komenta.be.model.playlist.PlayListDTO;
 
+import java.util.List;
+
 public interface PlayListMapper {
     int createPlayList(PlayListDTO dto);
+    List<PlayListDTO> getPlayListById(int u_id);
 }
