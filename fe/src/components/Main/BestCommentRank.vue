6e065f9c2d10b5dcaa8d38bdfd9ecbd4bd05283d
Hyerim Jeong <hrjeong8@gmail.com>
2021-02-08 00:08:03 +0900

    now playing postpone

--- fe/src/components/Main/BestCommentRank.vue
+++ fe/src/components/Main/BestCommentRank.vue
@@ -3,37 +3,26 @@
       <h4>실시간 베스트 댓글 유저</h4>
       <!-- 합칠 때 style에 있는 table width 속성 지우기 -->
       <table>
-        <thead>
-          <colgroup>
-            <col width="5%">
-            <col width="5%">
-            <col width="40%">
-            <col width="40%">
-            <col width="10%">
-          </colgroup>
-        </thead>
-        <tbody>
-          <tr v-if="bestComment.length == 0">
-            <td colspan="5">
-              가입한 회원이 없습니다.
-            </td>
-          </tr>
-          <tr v-for="(user,index) in bestComment" :key="index" v-else>
-            <td class="commRank-num" :class="{red: index < 3, nonHighLight: index >= 3}">{{index+1}}</td> 
-            <!-- :class="{red:fetchBestComment(index)}" -->
-            <td class="userPic">
-              <div>
-                <img :src="getProfile(index)" width="35px" alt="" />
-              </div>
-            </td>
-            <td>{{user.u_nickname}}</td>
-            <td>{{user.v_title}} {{user.ve_episode_num}}화</td>
-            <td>
-              <font-awesome-icon :icon="['fas', 'thumbs-up']" :style="{ color: '#fc3c44'}"/>
-              {{user.c_good_count}}
-            </td>
-          </tr>
-        </tbody>
+        <tr v-if="bestComment.length == 0">
+          <td colspan="5">
+            가입한 회원이 없습니다.
+          </td>
+        </tr>
+        <tr v-for="(user,index) in bestComment" :key="index" v-else>
+          <td class="commRank-num" :class="{red: index < 3, nonHighLight: index >= 3}">{{index+1}}</td> 
+          <!-- :class="{red:fetchBestComment(index)}" -->
+          <td class="userPic">
+            <div>
+              <img :src="getProfile(index)" width="35px" alt="" />
+            </div>
+          </td>
+          <td>{{user.u_nickname}}</td>
+          <td>{{user.v_title}} {{user.ve_episode_num}}화</td>
+          <td>
+            <font-awesome-icon :icon="['fas', 'thumbs-up']" :style="{ color: '#fc3c44'}"/>
+            {{user.c_good_count}}
+          </td>
+        </tr>
       </table>
   </div>
 </template>
@@ -117,7 +106,7 @@ export default {
 
 <style>
 table {
-  width:50%;
+  width:500px;
 }
 
 .commRank-num {
