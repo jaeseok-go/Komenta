2e782d7fcdb440674fe0f46a3f27dc9b6cdee165
배상웅 <dentist312@naver.com>
2021-02-08 15:25:05 +0900

    Picture path test

--- streaming/src/main/java/com/komenta/streaming/demo/controller/PictureController.java
+++ streaming/src/main/java/com/komenta/streaming/demo/controller/PictureController.java
@@ -2,10 +2,7 @@ package com.komenta.streaming.demo.controller;
 
 import org.apache.commons.io.IOUtils;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -19,12 +16,11 @@ public class PictureController {
             value = "/poster/{fileName}",
             produces = MediaType.IMAGE_JPEG_VALUE
     )
-    public byte[] getImageWithMediaType(@PathVariable("fileName") String fileName) throws IOException {
+    public @ResponseBody byte[] getImageWithMediaType(@PathVariable("fileName") String fileName) throws IOException {
         String path = Paths.get(System.getProperty("user.dir")).getFileSystem().getRootDirectories().iterator().next().toString();
         path += "home/ubuntu/Picture/Poster/";
         System.out.println(path+fileName+".jpg");
         InputStream in = getClass().getResourceAsStream(path+fileName+".jpg");
-        System.out.println(in.readAllBytes());
         return IOUtils.toByteArray(in);
     }
 
