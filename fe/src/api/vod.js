df2759131866f25cfc3b8ddb5f94562da1f198c7
박수아 <xlalfskdl@naver.com>
2021-02-08 12:52:05 +0900

    [ADD] vod,user api 추가

--- fe/src/api/vod.js
+++ fe/src/api/vod.js
@@ -118,21 +118,29 @@ function insertVodPoster(vodForm) {
      }
     })
 }
+
 // 플레이리스트 세부페이지
 function fetchPlayListDetail(plId) {
-    return instance.get('', {
+    return instance.get(`playlist/playlist_detail/${plId}`, {
         params: {
             pl_id: plId
         }
     })
 }
 
-// 좋아요가 많은 플레이리스트 순
+// 좋아요가 많은 플레이리스트 순(전체 플레이리스트 중 탑 10)
 function fetchPopularPlayList() {
-    return instance.get(`/playlist/bestplist`)
+    return instance.get(`playlist/bestplist`)
 }
 
-
+// 플레이리스트 컨텐츠의 상세보기
+function fetchPlaylistContent(plcId){
+    return instance.get(`playlist/contents_detail/${plcId}`,{
+        params:{
+            plc_id:plcId
+        }
+    })
+}
 
 export {
     fetchCommentVOD,
@@ -151,5 +159,6 @@ export {
     insertVOD,
     insertVodPoster,
     fetchPlayListDetail,
-    fetchPopularPlayList
+    fetchPopularPlayList,
+    fetchPlaylistContent
 }
