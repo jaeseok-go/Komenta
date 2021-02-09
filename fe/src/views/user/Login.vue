0b9b25c828e61311b65056b28beece852053aa2d
Hyerim Jeong <hrjeong8@gmail.com>
2021-02-10 00:48:10 +0900

    css modify

--- fe/src/views/user/Login.vue
+++ fe/src/views/user/Login.vue
@@ -1,8 +1,7 @@
 <template>
     <div>
         <div class="temp-bc-title">
-            <img src="@/imgs/3.png" alt="">
-            <h1 class="title-logo">Komenta</h1>
+            <img src="@/assets/images/KOMENTA_logo.png" alt="">
             <hr>
         </div>
         <!-- <button @click="signOut">Logout</button> -->
@@ -64,10 +63,10 @@ export default {
 </script>
 
 <style scoped>
-  .temp-bc-title {
+  /* .temp-bc-title {
     display: flex;
     background-color: #F9F9F9;
-  }
+  } */
 
   img {
     margin-left: 10px;
