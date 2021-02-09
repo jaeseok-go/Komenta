6dd795a2920fd4198cf159ce2f8e1c55e95ea23b
박수아 <xlalfskdl@naver.com>
2021-02-10 03:18:52 +0900

    [FEAT] category genres

--- fe/src/views/vod/Category.vue
+++ fe/src/views/vod/Category.vue
@@ -6,7 +6,7 @@
     </select>
     <select v-model="selectedGenreDetail">
         <option disabled value="">--세부 장르선택--</option>
-        <option v-for="genrdetail in genreDetails[selectedGenre]" :key="genrdetail.gd_id" :value="`${genrdetail.gd_id}`">{{genrdetail.gd_name}}</option>
+        <option v-for="genrdetail in subGenres" :key="genrdetail.gd_id" :value="`${genrdetail.gd_id}`">{{genrdetail.gd_name}}</option>
     </select>
     <div v-for='vod in vodlists' :key="vod.v_id">
     <span @click="goVodDetail(vod.v_id)">{{ vod.v_poster }}</span>
@@ -15,7 +15,7 @@
 </template>
 
 <script>
-// import {fetchAllGenre,fetchMainGenreVod, fetchSubGenreVod, fetchVodDetail} from '@/api/vod'
+import {fetchAllGenre,fetchMainGenreVod, fetchSubGenreVod, fetchVodDetail} from '@/api/vod'
 
 export default {
     name: 'Category',
@@ -23,67 +23,145 @@ export default {
         return {
             selectedGenre:'',
             selectedGenreDetail:'',
-            allGenres :[
+            allGenres :[],
+            vodlists:[],
+            subGenres:[],
+            genreDetails:[[
             {
-                "g_id": 1,
-                "g_name": "드라마"
+                "gd_id": 1,
+                "gd_name": "멜로",
+                "g_id": 1
             },
             {
-                "g_id": 2,
-                "g_name": "예능"
+                "gd_id": 2,
+                "gd_name": "스릴러",
+                "g_id": 1
             },
             {
-                "g_id": 3,
-                "g_name": "다큐"
+                "gd_id": 3,
+                "gd_name": "코미디",
+                "g_id": 1
             },
             {
-                "g_id": 4,
-                "g_name": "스포츠"
+                "gd_id": 4,
+                "gd_name": "액션",
+                "g_id": 1
+            }]
+            ,
+            [{
+                "gd_id": 5,
+                "gd_name": "음악",
+                "g_id": 2
             },
             {
-                "g_id": 5,
-                "g_name": "애니메이션"
-            }
-            ],
-            vodlists:[
-                [
-                {
-                    "g_name": "드라마",
-                    "gd_id": 0,
-                    "gd_name": "멜로",
-                    "v_actors": "string",
-                    "v_age_grade": 0,
-                    "v_director": "string",
-                    "v_id": 0,
-                    "v_poster": "string",
-                    "v_summary": "string",
-                    "v_title": "string"
-                }
-                ]
+                "gd_id": 6,
+                "gd_name": "버라이어티",
+                "g_id": 2
+            },
+            {
+                "gd_id": 7,
+                "gd_name": "토크",
+                "g_id": 2
+            },
+            {
+                "gd_id": 8,
+                "gd_name": "요리",
+                "g_id": 2
+            }]
+            ,
+
+            [{
+                "gd_id": 9,
+                "gd_name": "환경",
+                "g_id": 3
+            },
+            {
+                "gd_id": 10,
+                "gd_name": "역사",
+                "g_id": 3
+            },
+            {
+                "gd_id": 11,
+                "gd_name": "휴먼",
+                "g_id": 3
+            }]
+            ,
+
+            [{
+                "gd_id": 12,
+                "gd_name": "축구",
+                "g_id": 4
+            },
+            {
+                "gd_id": 13,
+                "gd_name": "농구",
+                "g_id": 4
+            },
+            {
+                "gd_id": 14,
+                "gd_name": "배구",
+                "g_id": 4
+            },
+            {
+                "gd_id": 15,
+                "gd_name": "야구",
+                "g_id": 4
+            }]
+            ,
+            [{
+                "gd_id": 16,
+                "gd_name": "로맨스/순정",
+                "g_id": 5
+            },
+            {
+                "gd_id": 17,
+                "gd_name": "스포츠",
+                "g_id": 5
+            },
+            {
+                "gd_id": 18,
+                "gd_name": "액션/추리",
+                "g_id": 5
+            },
+            {
+                "gd_id": 19,
+                "gd_name": "코미디",
+                "g_id": 5
+            }]
+
             ]
         };
     },
     created(){
-        // this.getAllGenre();
+        this.getAllGenre();
     },
     methods: {
-        getAllGenre() {
-            // this.allGenres = fetchAllGenre();
+        async getAllGenre() {
+            const genres = await fetchAllGenre();
+            this.allGenres = genres.data
+            console.log(this.allGenres,'모든장르')
         },
-        getGenreDetail(gId) {
-            // this.genreDetails = fetchMainGenreVod(gId);
-            // this.vodlists = fetchMainGenreVod(gId);
-            console.log(gId,'장르내용')
+        async getGenreDetail(gId) {
+            for (let i = 0; i < this.genreDetails.length; i++) {
+                const subgenre = this.genreDetails[i];
+                if (subgenre[0].g_id == gId) {
+                    this.subGenres = subgenre
+                }
+            }
+            const vod = await fetchMainGenreVod(gId);
+            this.vodlists = vod.data
+            console.log(gId,'장르내용',this.genreDetails, this.vodlists)
         },
-        getVodlist(gdId) {
-            // this.vodlists = fetchSubGenreVod(gdId);
+        async getVodlist(gdId) {
+            const vod = await fetchSubGenreVod(gdId);
+            this.vodlists = vod.data
             console.log(gdId,'장르세부내용')
         },
         async goVodDetail(vId){
             console.log(vId,'vod가장 첫 epi로 이동')
-            // const res = await fetchVodDetail(vId)
+            const res = await fetchVodDetail(vId)
             // VOD의 가장 첫 epi로 보내기
-            // this.$router.push(`/voddetail/${res.data[0].ve_id}`)
+            this.$router.push(`/voddetail/${res.data[0].ve_id}`)
         },
     },
     watch:{
