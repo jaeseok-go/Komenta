5af266b3965f1127664dd426aa0f605e5b834328
배상웅 <dentist312@naver.com>
2021-02-09 18:15:53 +0900

    Interceptor change

--- fe/src/api/config/interceptors.js
+++ fe/src/api/config/interceptors.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import store from '@/stores/index.js';
+import router from '@/routes/index.js';
+
 
 export function setInterceptors() {
   // let ddddd = store.state.user.token;
