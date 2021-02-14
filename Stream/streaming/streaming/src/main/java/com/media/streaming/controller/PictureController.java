7108e36bceb7c9b1d29f1fbdb110cf41384c96f9
배상웅 <dentist312@naver.com>
2021-02-15 01:13:13 +0900

    new Streaming Server

---
+++ Stream/streaming/streaming/src/main/java/com/media/streaming/controller/PictureController.java
@@ -0,0 +1,43 @@
+package com.media.streaming.controller;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
+
+@RestController
+@RequestMapping("/picture")
+public class PictureController {
+
+    @GetMapping(
+            value = "/poster/{fileName}",
+            produces = MediaType.IMAGE_JPEG_VALUE
+    )
+    public @ResponseBody byte[] getPosterImage(@PathVariable(name = "fileName") String fileName) throws IOException {
+        String path = Paths.get(System.getProperty("user.dir")).getFileSystem().getRootDirectories().iterator().next().toString();
+        path += "home/ubuntu/Picture/Poster/";
+        System.out.println(path+fileName+".jpg");
+        InputStream image = new FileInputStream(path+fileName+".jpg");
+        byte[] imageByte = IOUtils.toByteArray(image);
+        System.out.println(imageByte);
+        image.close();
+        return imageByte;
+    }
+    @GetMapping(
+            value = "/profile/{fileName}",
+            produces = MediaType.IMAGE_JPEG_VALUE
+    )
+    public @ResponseBody byte[] getProfileImage(@PathVariable(name = "fileName") String fileName) throws IOException {
+        String path = Paths.get(System.getProperty("user.dir")).getFileSystem().getRootDirectories().iterator().next().toString();
+        path += "home/ubuntu/Picture/User/";
+        System.out.println(path+fileName+".jpg");
+        InputStream image = new FileInputStream(path+fileName+".jpg");
+        byte[] imageByte = IOUtils.toByteArray(image);
+        System.out.println(imageByte);
+        image.close();
+        return imageByte;
+    }
+}
