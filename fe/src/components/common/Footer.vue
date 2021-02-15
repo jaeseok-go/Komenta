16c83a8cd01d956d6c65709dfb9092061303119d
박수아 <xlalfskdl@naver.com>
2021-02-16 05:07:00 +0900

    [FIX] footer modify

--- fe/src/components/common/Footer.vue
+++ fe/src/components/common/Footer.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-  <!-- <div class="footer-form">
+  <div class="footer-form">
     <div class="footer-content">
       <p>  (SSAFY 4기) 대전 2반 1팀 - Komenta |  팀장 : 배상웅  |  팀원 : 배상웅, 고재석, 김채린, 박수아, 정혜림</p>
       <p>본 프로젝트는 삼성전자 청년 SW 아카데미 교육의 일환으로써 어쩌구 저쩌구 무단 공유 및 배포를 엄격히 금합니다.</p>
     </div>
-  </div> -->
+  </div>
   
   </div>
 </template>
