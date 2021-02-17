13c9f9ed6ee06041061b7673ab3dbedcd5ced6ff
김채린 <kimchaelin13@gmail.com>
2021-02-17 21:25:58 +0900

    [FIX]어바웃페이지 수정

--- fe/src/components/Main/PopularPlayList.vue
+++ fe/src/components/Main/PopularPlayList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="popular-list">
-      <h4 class="page-title popular-list-title">골라보는 플레이리스트</h4>
+      <h4 class="page-title popular-list-title">골라보는 플레이리스트<div>인기있는 플레이리스트를 구독해보세요</div></h4>
       <!-- 플레이 리스트 타이틀, 플레이 리스트 좋아요 개수, 플레이 리스트 작성자, 플레이 리스트 안 컨텐츠 개수 -->
       <!-- pl_id, pl_name, 플레이 리스트 좋아요 개수, 플레이 리스트 작성자, 플레이 리스트 안 컨텐츠 개수-->
       <div class="btn-cover">
