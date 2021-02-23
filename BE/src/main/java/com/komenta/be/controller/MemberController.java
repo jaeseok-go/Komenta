be162eefbe7bdee28a03a355b554fb70ea726b3c
배상웅 <dentist312@naver.com>
2021-02-23 05:41:37 +0000

    Update BE/src/main/resources/application.yml, BE/src/main/java/com/komenta/be/controller/MemberController.java files Deleted BE/resources/이스터에그/be-0.0.1-SNAPSHOT.jar

--- BE/src/main/java/com/komenta/be/controller/MemberController.java
+++ BE/src/main/java/com/komenta/be/controller/MemberController.java
@@ -82,8 +82,8 @@ public class MemberController{
         }
 
         // SMS 인증 서비스 API env
-         String api_key = "NCSBCNFO2FFJM0BV";
-         String api_secret = "B1FFZEW61CGA5LW8MYV35WM7XYXZJNCB";
+         String api_key = "API KEY HERE";
+         String api_secret = "API SECRET HERE";
 //        String api_key = "";
 //        String api_secret = "";
         net.nurigo.java_sdk.api.Message coolsms = new Message(api_key, api_secret);
@@ -91,7 +91,7 @@ public class MemberController{
         // 4 params(to, from, type, text) are mandatory. must be filled
         HashMap<String, String> params = new HashMap<String, String>();
         params.put("to", u_phone_number);    // 수신전화번호
-        params.put("from", "01021147305");    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
+        params.put("from", "NUMBER HERE");    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
         params.put("type", "SMS");
         params.put("text", "Komenta : 인증번호는 " + "[ "+auth_number+" ]" + "입니다.");
         params.put("app_version", "test app 1.2"); // application name and version
