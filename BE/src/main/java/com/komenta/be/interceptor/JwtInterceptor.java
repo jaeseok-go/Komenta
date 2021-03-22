76345b4aa64ceeb39cc46d2bc61b7410b1cf655e
jaeseok-go <66155356+jaeseok-go@users.noreply.github.com>
2021-03-23 01:00:51 +0900

    Update JwtInterceptor.java
    [modify jwt interceptor] logger & 주석 수정

--- BE/src/main/java/com/komenta/be/interceptor/JwtInterceptor.java
+++ BE/src/main/java/com/komenta/be/interceptor/JwtInterceptor.java
@@ -21,30 +21,20 @@ public class JwtInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-//        logger.info(request.getParameter("u_email")+ " " +
-//                request.getParameter("u_nickname")+ " " +
-//                request.getParameter("u_phone_number"));
 
-//        logger.info(request.getMethod() + " : " + request.getServletPath());
         if(request.getMethod().equals("OPTIONS")) {
-//            System.out.println("option 맞냐");
             return true;
         }
-        // request의 parameter에서 auth_token으로 넘어온 녀석을 찾아본다.
-        // String token = request.getParameter("auth_token");
+        // request의 param에서 auth-token을 찾아본다.
         String token = request.getHeader("auth-token");
-//        String test = response.getHeader("auth-token");
-//        System.out.println("test : "+test);
-        if (token!=null && token.length()>0) {
-            // 유효한 토큰이면 진행, 그렇지 않으면 예외를 발생시킨다.
-//            jwtService.checkValid(token);
+        
+        // 유효한 토큰이면 진행, 그렇지 않으면 예외 발생
+        if (token != null && token.length() > 0) {
             Jwts.parser().setSigningKey("VUETOKEN".getBytes()).parseClaimsJws(token);
-//            logger.info("토큰 사용 가능 : {}", token);
             return true;
         }
         else {
-            System.out.println("토큰없습니다.");
-//            throw new RuntimeException("인증 토큰이 없습니다.");
+            throw new RuntimeException("인증 토큰이 없습니다.");
             return false;
         }
     }
